f596f2689f131e558807c14d35e9635d5155cd77
dev2-sedi-equipement <dev2@sedi-equipement.fr>
2024-06-14 10:02:50 +0200

    Installations des dépendances

---
+++ vendor/symfony/ux-live-component/assets/dist/PollingDirector.d.ts
@@ -0,0 +1,16 @@
+import Component from './Component';
+export default class {
+    component: Component;
+    isPollingActive: boolean;
+    polls: Array<{
+        actionName: string;
+        duration: number;
+    }>;
+    pollingIntervals: number[];
+    constructor(component: Component);
+    addPoll(actionName: string, duration: number): void;
+    startAllPolling(): void;
+    stopAllPolling(): void;
+    clearPolling(): void;
+    private initiatePoll;
+}
